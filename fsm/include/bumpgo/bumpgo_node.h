0319fd667cf6ac3112f9a452bbc1b7f4af53a840
beacon lee <bquanlicn@gmail.com>
2024-06-12 20:27:59 +0900

    feat: 添加 execute node

--- fsm/include/bumpgo/bumpgo_node.h
+++ fsm/include/bumpgo/bumpgo_node.h
@@ -3,7 +3,7 @@
 #include <geometry_msgs/msg/twist.hpp>
 
 
-namespace fsm::bumpgo
+namespace fsm::bump_go
 {
 
 class BumpGoNode : public rclcpp::Node
@@ -13,19 +13,17 @@ public:
     : rclcpp::Node(std::move(node_name))
   {
     publisher_ = create_publisher<geometry_msgs::msg::Twist>("/twist", 10);
+
     subscription_ = create_subscription<sensor_msgs::msg::LaserScan>(
         "/layse_scan",
         10,
         std::bind(&BumpGoNode::ReceiveLaserScan, this, std::placeholders::_1));
+
     timer_ = create_wall_timer(std::chrono::milliseconds(50),
-                               std::bind(&BumpGoNode::ControlCircle, this));
+                               std::bind(&BumpGoNode::ControlCycle, this));
   }
 
 private:
-  void ReceiveLaserScan(sensor_msgs::msg::LaserScan::UniquePtr msg);
-
-  void ControlCircle();
-
   enum class RobotState
   {
     FORWARD,
@@ -34,13 +32,22 @@ private:
     STOP,
   };
 
+  void ReceiveLaserScan(sensor_msgs::msg::LaserScan::UniquePtr laser_scan);
+
+  void ControlCycle();
+
+  void Go2State(RobotState new_state);
+
+
   RobotState state_{BumpGoNode::RobotState::STOP};
 
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
 
   rclcpp::TimerBase::SharedPtr timer_;
-  sensor_msgs::msg::LaserScan::UniquePtr last_;
+
+  rclcpp::Time state_ts_; // ts is timestamp 时间戳
+  sensor_msgs::msg::LaserScan::UniquePtr last_scan_;
 };
 
-} // namespace fsm::bumpgo
+} // namespace fsm::bump_go
--- fsm/src/bumpgo/bumpgo_node.cc
+++ fsm/src/bumpgo/bumpgo_node.cc
@@ -3,4 +3,46 @@
 namespace fsm::bump_go
 {
 
+void BumpGoNode::ReceiveLaserScan(
+    sensor_msgs::msg::LaserScan::UniquePtr laser_scan)
+{
+  last_scan_ = std::move(laser_scan);
 }
+
+void BumpGoNode::ControlCycle()
+{
+  if(!last_scan_)
+  {
+    return;
+  }
+
+  geometry_msgs::msg::Twist twist;
+
+  switch(state_)
+  {
+    case RobotState::FORWARD:
+      twist.linear.set__x(0.5);
+      break;
+    case RobotState::BACK:
+      twist.linear.set__x(-0.5);
+      break;
+    case RobotState::STOP:
+
+      break;
+    case RobotState::TURN:
+      twist.angular.set__z(-0.1);
+      break;
+    default:
+      break;
+  }
+
+  publisher_->publish(twist);
+}
+
+
+void BumpGoNode::Go2State(RobotState new_state)
+{
+  state_ = new_state;
+  state_ts_ = now();
+}
+} // namespace fsm::bump_go
