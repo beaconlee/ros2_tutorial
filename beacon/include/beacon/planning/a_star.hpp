855a340b7688bf37f25d8231552bfa01890a2b8b
beacon lee <bquanlicn@gmail.com>
2024-06-28 20:12:44 +0900

    feat:添加 a_star 基础框架

---
+++ beacon/include/beacon/planning/a_star.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+namespace beacon
+{
+namespace planning
+{
+class AStar
+{
+public:
+private:
+};
+} // namespace planning
+} // namespace beacon
---
+++ beacon/src/planning/a_star.cc
@@ -0,0 +1,10 @@
+#include "beacon/planning/a_star.hpp"
+
+
+int main()
+{
+  beacon::planning::AStar a;
+  
+
+  return 0;
+}
