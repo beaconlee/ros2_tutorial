46772550ceb1c5db94de3979858f3108b8fdfc07
beacon lee <bquanlicn@gmail.com>
2024-09-10 15:20:17 +0900

    feat:修改进入docker脚本,设置进入的目录

--- enter_docker.sh
+++ enter_docker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # 进入正在运行的容器
-docker exec -it beaconbots /bin/bash
+# -w /workspace：这将指定进入容器时的工作目录为 /workspace。
+# 这样写有问题, -w 要在前面
+# docker exec -it beaconbots -w /workspace /bin/bash
+
+docker exec -it -w /workspace beaconbots /bin/bash
