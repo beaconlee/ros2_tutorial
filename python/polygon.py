9e9da287a1458a87a7ce887c5f24a9bcac71e7cc
beacon lee <bquanlicn@gmail.com>
2024-10-06 03:02:17 +0000

    feat:matplotlib设置轴等比例

--- python/polygon.py
+++ python/polygon.py
@@ -5,7 +5,7 @@ points = [
   [2, 1],
   [3, 1.5],
   [4, 2],
-  [2, 4],
+  [2, 10],
   [1, 2]
 ]
 
@@ -19,5 +19,9 @@ ax.add_patch(pgon)
 # ax.set_ylim(0, 5)
 ax.autoscale()
 
+# 使用 matplotlib 进行绘图时,默认情况下，坐标轴的纵横比会根据数据自动调整，可能导致图形看起来失真。
+# ax.set_aspect('equal') 用于设置坐标轴的纵横比，使得x轴和y轴的比例相等。
+# 换句话说，1个单位的长度在x方向和y方向上将显示为相同的物理距离（无论坐标轴的刻度如何）。
+ax.set_aspect('equal')
 
 plt.show()
