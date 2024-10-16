bb777d93e95c5d9dc2966890e66280c3ec8c6e21
beacon lee <bquanlicn@gmail.com>
2024-10-16 01:58:04 +0000

    style:format

--- python/log.py
+++ python/log.py
@@ -10,9 +10,10 @@ class LogColors:
 
 # 自定义日志处理器
 class ColoredFormatter(logging.Formatter):
-    def __init__(self, fmt):
-        super().__init__()
+    def __init__(self, fmt, datefmt=None):
+        super().__init__(fmt=fmt, datefmt=datefmt)
         self.fmt = fmt
+        self.datefmt = datefmt  # 保存 datefmt 以供 format 方法使用
         self.FORMATS = {
             logging.DEBUG: f"{LogColors.BLUE}{self.fmt}{LogColors.RESET}",
             logging.INFO: f"{LogColors.GREEN}{self.fmt}{LogColors.RESET}",
@@ -25,7 +26,8 @@ class ColoredFormatter(logging.Formatter):
         # 对齐日志级别，固定宽度为 8（比如 'CRITICAL' 是 8 个字符）
         record.levelname = record.levelname.ljust(8)
         log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
+        # formatter = logging.Formatter(log_fmt)
+        formatter = logging.Formatter(log_fmt, datefmt=self.datefmt)
         return formatter.format(record)
 
 # 单例的 CustomLogger 实现
@@ -51,8 +53,8 @@ class CustomLogger:
             # 定义日志输出格式，使用 logging 的格式化占位符
             # 如果不加 % 和 s 的话不会格式化替换输出
             # log_format = "{asctime} - {levelname} - {filename}:{lineno} - {message}"
-            log_format = "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
-            date_format = "%Y-%m-%d %H:%M:%S"  # 设置不显示毫秒
+            log_format = "%(asctime)s %(filename)s:%(lineno)d %(levelname)s: %(message)s"
+            date_format = "%d %H:%M:%S"  # 设置时间格式
 
             formatter = ColoredFormatter(fmt=log_format, datefmt=date_format)
 
